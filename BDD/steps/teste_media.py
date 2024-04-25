449332131aef0340814e58a07a343707675a5945
Victor Naoki Sato <victornaokisatosato@gmail.com>
2024-04-25 20:52:29 -0300

    atividade agil versao 2

--- BDD/steps/teste_media.py
+++ BDD/steps/teste_media.py
@@ -1,11 +1,11 @@
 from behave import given, when, then
 
-@given('eu tenho dois números: {num1:f} e {num2:f}')
+@given('eu tenho dois números flutuantes: 5 e 7 : {num1:f} e {num2:f}')
 def step_impl(context, num1, num2):
     context.num1 = num1
     context.num2 = num2
 
-@when('eu somo os dois números e divido por dois ')
+@when('eu somo os dois números flutuantes e divido por dois')
 def step_impl(context):
     context.resultado = (context.num1 + context.num2)/2
 
